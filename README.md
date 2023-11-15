ee5efee3b2ec99d7da7c433c03034ee911a17484
shamito <shamito@google.com>
2023-11-15 13:49:22 -0600

    reinit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProjectForm
+# App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'project-form' title`, () => {
+  it(`should have the 'app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-form');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, project-form');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app');
   });
 });
